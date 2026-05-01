700093c1412ac8c4a6b01a5d4ae7d8126574a62b
Dominik Hoffmann <cycrow00@gmail.com>
2026-05-01 15:07:25 +0200

    feat: implement base Plugin class with state machine for automated gear optimization

--- RoleGearSync/obj/Debug/RoleGearSync.AssemblyInfo.cs
+++ RoleGearSync/obj/Debug/RoleGearSync.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Synchronisiert Ausrüstung für FFXIV Rollen.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0.0+ea8653c31c2acc75e28134d8e76085cc834d392c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0.0+c45efad3bd06979366be5e98e26c7d5871a0a5f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoleGearSync")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoleGearSync")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
