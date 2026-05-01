7048c43c451321a10473afa7fb1cd3c898dab597
Dominik Hoffmann <cycrow00@gmail.com>
2026-05-01 17:16:56 +0200

    build: add assembly information and source link configuration for debug build

--- RoleGearSync/obj/Debug/RoleGearSync.AssemblyInfo.cs
+++ RoleGearSync/obj/Debug/RoleGearSync.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Synchronizes gear sets for all jobs within a role.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0.0+a5f0faf1c72273a41ea2f203ee55139eb6eb68e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0.0+635c5c598231e90ab158b9157f2b2fc491724f55")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoleGearSync")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoleGearSync")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
