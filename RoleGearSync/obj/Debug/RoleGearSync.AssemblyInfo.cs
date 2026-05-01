07bde1e2b34dfceb4bbd0b1c0139023acbf18a82
Dominik Hoffmann <cycrow00@gmail.com>
2026-05-01 15:47:18 +0200

    build: generate assembly information and source link mapping for project build artifacts

--- RoleGearSync/obj/Debug/RoleGearSync.AssemblyInfo.cs
+++ RoleGearSync/obj/Debug/RoleGearSync.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Synchronisiert Ausrüstung für FFXIV Rollen.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0.0+48447d17e8a6b37635ebf843c542a66a0512088f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0.0+28865588c24af74dccc0747e3697d1756b887378")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoleGearSync")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoleGearSync")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
